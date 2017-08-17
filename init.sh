850e3ca6307296ea16d92650a317b7c75a743011
piv <pivcheg000@gmail.com>
2017-08-17 12:30:21 +0700

    Fix some problems

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ sudo /bin/ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo /bin/ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-﻿sudo /etc/init.d/mysql start﻿
+﻿sudo /etc/init.d/mysql restart﻿
