534904657dd66c336aba03c370e24f7f2f304789
piv <pivcheg000@gmail.com>
2017-08-28 14:24:20 +0700

    Added conf files for gunicorn and supervisord

--- init.sh
+++ init.sh
@@ -3,7 +3,12 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo /bin/ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo /bin/ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
-﻿sudo /etc/init.d/mysql restart﻿
+#sudo /bin/ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
+#﻿sudo /etc/init.d/mysql restart﻿
 sudo apt remove python-django
+sudo apt remove gunicorn
+sudo apt install python3-pip
+sudo pip3 install gunicorn
+sudo apt install supervisor
+sudo ln -s /home/box/web/etc/supervisor.conf /etc/supervisor/conf.d/ask.conf
