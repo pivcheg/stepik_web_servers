bae5b08dbd956319fbd8a3bbd2e7fb092afe0ed9
piv <pivcheg000@gmail.com>
2017-09-06 12:42:13 +0700

    Added models in file qa/models.py

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ sudo /bin/ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 #sudo /bin/ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
-#﻿sudo /etc/init.d/mysql restart﻿
+﻿sudo /etc/init.d/mysql restart﻿
 sudo apt update
 sudo apt remove python-django -y
 sudo apt remove gunicorn -y
@@ -13,6 +13,7 @@ sudo apt install mc -y
 sudo apt install python3-pip -y
 sudo apt install python2.7 -y
 sudo apt install supervisor -y
+sudo service supervisor start
 sudo pip3 install gunicorn
 sudo pip3 install django
 sudo cp /home/box/web/etc/supervisord.conf /etc/supervisor/supervisord.conf
