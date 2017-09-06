31b9d831a4f963378cd2a185bd9aa662402ae8ae
piv <pivcheg000@gmail.com>
2017-09-06 13:24:44 +0700

    Changed init.sh

--- init.sh
+++ init.sh
@@ -2,10 +2,10 @@
 
 sudo rm /etc/nginx/sites-enabled/default
 sudo /bin/ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
+sudo service nginx restart
 #sudo /bin/ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
-﻿sudo /etc/init.d/mysql restart﻿
+﻿sudo service mysql restart﻿
 sudo apt update
 sudo apt remove python-django -y
 sudo apt remove gunicorn -y
@@ -13,11 +13,11 @@ sudo apt install mc -y
 sudo apt install python3-pip -y
 sudo apt install python2.7 -y
 sudo apt install supervisor -y
-sudo service supervisor start
 sudo pip3 install gunicorn
 sudo pip3 install django
 sudo cp /home/box/web/etc/supervisord.conf /etc/supervisor/supervisord.conf
 sudo ln -s /home/box/web/etc/ask.conf /etc/supervisor/conf.d/ask.conf
+sudo service supervisor start
 sudo supervisorctl reread
 sudo supervisorctl update
 sudo supervisorctl restart ask
