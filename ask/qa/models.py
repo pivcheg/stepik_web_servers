65d442dbaaeb37c2c5c0768d52a8dd68b435139e
piv <pivcheg000@gmail.com>
2017-09-08 09:54:05 +0700

    Some changes for test

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,23 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Question(models.Model):
-    title = models.CharField(max_length=255, blank=False)
-    text = models.TextField(blank=False)
-    added_at = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User, default='x')
-    likes = models.ManyToManyField(User, related_name='question_like_user', blank=True)
 
-class QuestionManager(Question):
+class QuestionManager(models.Manager):
     def new(self):
         return self.ordering('-added_at')
 
     def popular(self):
         return self.ordering('-rating')
 
+
+class Question(models.Model):
+    objects = QuestionManager()
+    title = models.CharField(max_length=255, blank=False)
+    text = models.TextField(blank=False)
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User)
+    likes = models.ManyToManyField(User, related_name='question_like_user', blank=True)
+
+
 class Answer(models.Model):
     text = models.TextField(blank=False)
     added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question, blank=False, on_delete=models.PROTECT)
-    author = models.ForeignKey(User, default='x')
+    author = models.ForeignKey(User)
