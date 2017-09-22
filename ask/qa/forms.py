f631ebdd458f127d3b9f279420048e45cffe0c66
piv <pivcheg000@gmail.com>
2017-09-22 14:22:48 +0700

    Some changes in forms.py

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -7,11 +7,18 @@ class AskForm(forms.Form):
     title = forms.CharField(max_length=100, label="Заголовок вопроса")
     text = forms.CharField(widget=forms.Textarea, label="Вопрос")
 
+    def clean_title(self):
+        print(self.cleaned_data['title'])
+        title = self.cleaned_data['title']
+        return title
+
     def clean_text(self):
+        print(self.cleaned_data['text'])
         text = self.cleaned_data['text']
         return text
 
     def save(self):
+        print("Save:", self)
         question = models.Question(**self.cleaned_data)
         question.author, _ = models.User.objects.get_or_create(username='anon')
         print(models.User.objects.get_or_create(username='anon'))
@@ -25,22 +32,28 @@ class AnswerForm(forms.Form):
     text = forms.CharField(widget=forms.Textarea, label="Комментарий")
 
     def __init__(self, *args, **kwargs):
-        # print("__init__", "queryset:", quest_object, "args:", args, "kwargs", kwargs)
         super(AnswerForm, self).__init__(*args, **kwargs)
         self._qid = kwargs.get('initial', None)
+
         if self._qid is not None:
             self._qid = self._qid.get('qid', None)
+
         print("__init__", "self._qid:", self._qid, "args:", args, "kwargs", kwargs)
-        # self._quest = quest_object
+
+    def clean_question(self):
+        print(self.cleaned_data['question'])
+        question = self.cleaned_data['question']
+        return question
 
     def clean_text(self):
+        print(self.cleaned_data['text'])
         text = self.cleaned_data['text']
         return text
 
     def save(self):
-        print("save:", self)
         self.cleaned_data['question'] = self._qid
         self.cleaned_data['author'], _ = models.User.objects.get_or_create(username='anon')
         answer = models.Answer(**self.cleaned_data)
         answer.save()
+        print("save:", self)
         return answer
