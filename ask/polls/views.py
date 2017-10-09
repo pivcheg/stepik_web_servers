89ea80da34a9a814045865eee964df31907fcc95
piv <pivcheg000@gmail.com>
2017-10-09 16:58:44 +0700

    Added init_data.sh. This script make migrations. Added templates for the application polls. Changed urls.py. Added some new routes.

--- ask/polls/views.py
+++ ask/polls/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, polls index!")
+    latest_questions_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_questions_list': latest_questions_list}
+    return render(request, "polls/index.html", context)
 
+def detail(request, question_id):
+    return HttpResponse("Question %s" % question_id)
+
+def results(request, question_id):
+    response = "Results of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Voting question %s" % question_id)
