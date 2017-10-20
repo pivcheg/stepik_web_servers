9f1ec9a059f0129ed5cfba3bd6d6c13b75c1afa3
ogurtk01 <pivcheg000@gmail.com>
2017-10-20 16:47:13 +0700

    Added: the template for polls. Changed: models.py for polls.

--- ask/polls/models.py
+++ ask/polls/models.py
@@ -9,7 +9,7 @@ class Question(models.Model):
     Модель для создания вопросов.
     """
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published', default=timezone.now())
+    pub_date = models.DateTimeField('date published', default=timezone.now)
 
     def __str__(self):
         return self.question_text
@@ -36,7 +36,7 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    add_date = models.DateTimeField(default=timezone.now())
+    add_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.choice_text
